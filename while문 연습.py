7ab93592a12b5c5a789da0579ca9e9c9fc62fcdc
김진희 <KimJinHui@email.com>
2025-09-11 17:50:18 +0900

    while문 연습

---
+++ while문 연습.py
@@ -0,0 +1,12 @@
+running = True
+i = 0
+while running:
+     print('집갈래')
+     i += 1
+     if i == 5:
+         running = False
+
+running = True
+while running:
+    print('집갈래')
+    running = False
