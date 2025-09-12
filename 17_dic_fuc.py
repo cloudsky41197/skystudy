eacdbf98be047398c00d3a772e29c38cd5a70d31
김진희 <KimJinHui@email.com>
2025-09-12 12:20:08 +0900

    dic 함수소개-문제

--- 17_dic_fuc.py
+++ 17_dic_fuc.py
@@ -9,3 +9,54 @@ print(dic.keys())
 
 for item in dic.keys():
     print(item)
+
+#dict_keys -> list 로 변환
+keys = list(dic.keys());
+print(keys)
+
+# dic.values() : 특정 사전의 값만 가져와 dict_values 라는 객체를 반환
+print(dic.values())
+# list 로 변경해서 values 라는 변수에 담아보자
+
+# keys = list(dic.keys());
+# print(keys)
+#
+# print(dic.values(['Alice','John','Smith']); # 다 틀림
+
+values = list(dic.values())
+print(values)
+
+# dic.items() : 사전의 키:값을 한 쌍으로 가져와 dict_items 로 반환한다.
+# 각 키와 값은 () 모양으로 보아 tuple 이다.
+print(dic.items())
+# list 로 변환해 보면 list 안에 각 키와 값이 튜플로 저장되어 있음을 알 수 있다.
+li = list(dic.items())
+print(li)
+
+# 값을 가져오기 (택1)
+print(dic.get('name'))
+print(dic['phone'])
+
+# dic 안에 있는 모든 키와 값을 키 : 값 형태로 출력해 보자
+#print(dic.get('age'))
+#print(dic.('phone':'010-1234-1234'));
+#print(dic.('friends'))
+
+# 1. 키를 뽑아낸 다음, 키를 가지고 값을 뽑아내는 방법
+
+for key in dic.keys():
+    #print(key)
+    print(f'{key}:{dic[key]}')
+
+# 2. 키와 값을 동시에 뽑아낸 다음 거기서 키와 값을 각각 추출하는 방식
+
+for item in dic.items():
+    print(f'{item[0]}={item[1]}')
+
+# print(dic.get('name'));
+# print(dic.get('phone'));
+# print(dic.get('friends'));
+#
+# print(f'{} : {dic['name']}');
+
+
