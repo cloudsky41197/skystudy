e4d87e1e907fb7983331a216cc1a31560731134d
김진희 <KimJinHui@email.com>
2025-09-16 15:36:59 +0900

    예외처리

--- advance/11_handling.py
+++ advance/11_handling.py
@@ -1,4 +1,4 @@
-num_list = [1,2,3,1,2,3,1,2,3,4,5,6]
+num_list = [1,2,3,1,2,3,1,2,3,1,2,3,4,5,6]
 
 idx = 0
 
@@ -7,7 +7,8 @@ try:
         idx = num_list.index(3,idx)
         print(f'3은 {idx} 인덱스에 있습니다.')
         idx += 1
-except ValueError:
+except ValueError as e:
+    print(e) # 예외에 대한 대략적인 정보 출력
     print('더이상 3을 찾을 수 없습니다.')
 finally:
     print('====끝====')
