49c996689dce69e9f4f5d96c95c12054b422a10b
김진희 <KimJinHui@email.com>
2025-09-16 12:05:47 +0900

    점퍼만들기

---
+++ advance/08_inheri.py
@@ -0,0 +1,13 @@
+class Runner :
+    def run(self):
+        pass
+    def sprint(self):
+        pass
+class Jumper:
+    def jump(self):
+        pass
+    def high_jump(self):
+        pass
+
+class person(Jumper, Runner):
+    pass
