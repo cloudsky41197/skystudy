40e91855835278c332d77153a47e9a0ac51d0edf
김진희 <KimJinHui@email.com>
2025-09-16 12:56:25 +0900

    생성자 전달 순서 설명

--- advance/09_super_init.py
+++ advance/09_super_init.py
@@ -23,8 +23,11 @@ class Teacher(SchoolMember): # 자식
     salary = 0
 
     def __init__(self, name, age, salary):
-        super().__init__(name, age)
-        self.salary = salary
+        super().__init__(name, age) # 2. 부모를먼저 객체화 시키면서 초기화할 값을 전달
+        self.salary = salary # 4. 그리고 나서 내각 초기화  하면서 객체화 된다.
+
 
 t = Teacher('김철수',33,500000000)
 print(f'{t.name}({t.age})-{t.salary}')
+# 5. name 과 age 는 부모것 이지만 내것처럼 내 객체에서 가져다 쓸 수 있게 된다.
+print(f'{t.name}({t.age}) - {t.salary}')
